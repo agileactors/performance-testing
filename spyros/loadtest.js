2277c4ac78be0311104c84340924c66b60940215
Spyros Vlachos <spyros.vlachos3@vodafone.com>
2023-10-19 12:27:30 +0300

    this branch contains the .js file for the loadtest

--- spyros/loadtest.js
+++ spyros/loadtest.js
@@ -1,1 +1,9 @@
-this is a test file
+import { check } from 'k6';
+import http from 'k6/http';
+export default function () {
+  const res = http.get('http://cosmote.gr/hub');
+  check(res, {
+    'is body ok': (r) => r.body.includes('ote'),
+        'status is 200:': (r)=> r.status===200,
+  });
+}
